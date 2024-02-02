dd0553bb0aaef79d9892926a6738d9b19ea0510a
edukso <158623858+edukso@users.noreply.github.com>
2024-02-02 19:57:23 -0300

    Add files via upload

---
+++ 1-Coursework.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Data Science Tools and Ecosystem
+
+# Author
+# 
+# Eduardo Kronbauer Soares
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# - List popular data science tools.
+# - Use Python to convert minutes to hours.
+# - List popular data science languages.
+
+# Some of the popular languages that Data Scientists use are:
+# - Python
+# - R
+# - SQL
+
+# Some of the commonly used libraries used by Data Scientists include:
+# - ggplot(R)
+# - pandas(python)
+# - scipy (python)
+
+# | Tools                   |
+# |-------------------------|
+# | Jupyter Notebooks       |
+# | Python                  |
+# | R                       |
+
+# Below are a few examples of evaluating arithmetic expressions in Python.
+
+# In[1]:
+
+
+(3*4)+5
+
+
+# This will convert 240 minutes to hours by diving by 60.
+
+# In[4]:
+
+
+min=240
+hour=min/60
+print(hour)
+
+
+# In[ ]:
+
+
+
+
